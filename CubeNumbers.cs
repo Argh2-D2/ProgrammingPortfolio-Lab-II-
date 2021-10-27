0b36b4a6311fd3d423e5764dacd7bf1d59406be2
Jordan J. Mugenyi <jordanjamesxvi@outlook.com>
2021-10-27 09:30:34 +0100

    Add files via upload

---
+++ CubeNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace L2._1_Cubenumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number you would like to be cubed.");
+            int input = int.Parse(Console.ReadLine());
+            int cubeNumber = input * input * input;
+            int x = cubeNumber;
+            Console.WriteLine(input + " cubed is " + x);
+        }
+    }
+}
